6d0cc9001a04abc4cbfda22fbfbd1a59a17bf120
StinkySteak <slimesteve17@gmail.com>
2025-09-06 09:59:17 +0700

    feat: remove signaling server from unity

--- Runtime/SignalingServer/Session.cs
+++ Runtime/SignalingServer/Session.cs
@@ -2,10 +2,5 @@ using System;
 
 namespace Netick.Transport.WebRTC
 {
-    public struct Session
-    {
-        public string JoinCode;
-        public int OwnerConnectionId;
-        public DateTime LastHeartbeatTime;
-    }
+    
 }
